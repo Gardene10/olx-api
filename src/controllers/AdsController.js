69e24d90b39b44c9dc5267086d4c1cfc4c2160b9
Gardene 10 <gardene.v.c@gmail.com>
2023-08-03 18:38:37 -0300

    adding categories and  its own images

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -1,7 +1,20 @@
+//usando o model no controller
+const Category = require('../models/Category')
+
 module.exports = {
     // funcoes de Ads para o controller
     getCategories: async (req, res) => {
+        const cats = await Category.find()
 
+        let categories = []
+        for (let i in cats){
+            categories.push({
+                ...cats[i]._doc,
+                img: `${process.env.BASE}/assents/images/${cats[i].slug}.png`
+            })
+        }
+
+        res.json({categories})
     },
     addAds: async (req, res) => {
 
